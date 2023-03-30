a1d49494e8f546f70ff9f699eede7082d44aee47
00011991 <117220967+00011991@users.noreply.github.com>
2023-03-30 12:56:38 +0500

    changed validation

--- routes/new-post.js
+++ routes/new-post.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const Post = require('../models/Post');
 const fs = require('fs')
 
-
+const clearSession = require('../middlewares/clearSession')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/uploads")
@@ -29,9 +29,10 @@ router.route('/')
     .get(
         async (req, res) => {
             const posts = await Post.find({});
-
+            var err = req.session.err;
             res.render("index", { posts })
         })
+
 router.route('/new')
     .get((req, res) => {
         var err = req.session.err;
@@ -43,43 +44,59 @@ router.route('/new')
             await post.save()
             res.redirect('/')
         } catch (error) {
-            next(error)
+
+            res.render('newPost', { err: error })
         }
 
     })
 
 router.route('/post/:id')
-    .get(async (req, res) => {
-        const post = await Post.findById({ _id: req.params.id })
-        res.render('postDetails', { post })
+    .get(clearSession, async (req, res, next) => {
+        try {
+            const post = await Post.findById({ _id: req.params.id })
+            if (!post) {
+                req.flash('error', "Post Not Found")
+                throw new Error("Post Not Found")
+            }
+            var err = req.session.err;
+            res.render('postDetails', { post, err })
+        } catch (error) {
+            next(error)
+        }
+
     })
     .delete(async (req, res) => {
         const post = await Post.findByIdAndDelete(req.params.id)
         post.path.length !== 0 && await deleteFile(`public/uploads/${post.path}`)
         res.redirect(`/`)
     })
-    .put(upload.single('image'), async (req, res) => {
+    .put(upload.single('image'), async (req, res, next) => {
         const { id } = req.params;
-        const { title, desc } = req.body
-        const post = await Post.findById(id);
-        post.title = title;
-        post.desc = desc;
-        if (req.file) {
-            if (post.path.length > 0) {
-                await deleteFile(`public/uploads/${post.path}`);
+        try {
+            const { title, desc } = req.body
+            const post = await Post.findById(id);
+            post.title = title;
+            post.desc = desc;
+            if (req.file) {
+                if (post.path.length > 0) {
+                    await deleteFile(`public/uploads/${post.path}`);
+                }
+                post.path = req.file.filename;
             }
-            post.path = req.file.filename;
+            await post.save();
+            res.redirect(`/post/${req.params.id}`)
+        } catch (error) {
+            req.session.err = error;
+
+            res.redirect(`/post/edit/${id}`)
         }
-        await post.save();
-        res.redirect(`/post/${req.params.id}`)
+
     })
 
 router.route('/post/edit/:id')
     .get(async (req, res) => {
         const post = await Post.findById({ _id: req.params.id })
-        res.render('editPost', { post })
+        var err = req.session.err;
+        res.render('editPost', { post, err })
     })
-
-
-
 module.exports = router;
